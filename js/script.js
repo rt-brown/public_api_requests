6edd0d6a7f0e1c6688d405771be7ceb15b1eb757
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-11-07 15:18:38 -0700

    created fetchData and starting generateHTML

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-ls
-/
-clear
+let jsonData
+
+function fetchData(url){
+    return fetch(url)
+    .then(response => response.json())
+    
+}
+
+function generateHTML (data){
+    data.forEach(element => {
+        let personFirstName = element.name.first
+        let personLastName = element.name.last
+        let personEmail = element.email
+        let personCity = element.location.city
+        let personImage = element.picture.thumbnail 
+
+        console.log(personFirstName, personLastName, personEmail, personCity, personImage);
+    });
+
+}
+
+fetchData('https://randomuser.me/api/?results=12')
+    .then(data => data.results)
+    .then(generateHTML);
+
+    //console.log(jsonData);
