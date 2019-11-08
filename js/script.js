8968e23a7e3296f1620094ffbf70b146a7dba4b9
rt-brown <47324919+rt-brown@users.noreply.github.com>
2019-11-08 14:05:10 -0700

    finished generateHTML

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-let jsonData
 
 function fetchData(url){
     return fetch(url)
     .then(response => response.json())
-    
 }
 
 function generateHTML (data){
+    const gallery = document.getElementById('gallery');
     data.forEach(element => {
-        let personFirstName = element.name.first
-        let personLastName = element.name.last
-        let personEmail = element.email
-        let personCity = element.location.city
-        let personImage = element.picture.thumbnail 
 
-        console.log(personFirstName, personLastName, personEmail, personCity, personImage);
+        let HTML = `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src=${element.picture.thumbnail} alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
+            <p class="card-text">${element.email}</p>
+            <p class="card-text cap">${element.location.city}, ${element.location.state}</p>
+        </div>
+    </div>`
+
+    gallery.innerHTML += HTML;
+        
     });
-
+   
 }
 
 fetchData('https://randomuser.me/api/?results=12')
     .then(data => data.results)
     .then(generateHTML);
 
-    //console.log(jsonData);
+    
